967344f972a9198488b25c224ac50209fbc5311e
b915427386 <b915427386@gmail.com>
2018-09-25 21:34:14 +0800

    aws001

--- build_service.sh
+++ build_service.sh
@@ -5,7 +5,7 @@ $(aws ecr get-login --no-include-email --region ap-northeast-1)
 docker pull 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/api:latest
 docker pull 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/mysql:latest
 docker pull 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/redis:latest
-docker pull 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/jupyter:0.0.2
+docker pull 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/jupyter:latest
 docker pull 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/ngrok:latest
 echo "download S3 cc102-csv data "
 cd /home/ec2-user/deploy
