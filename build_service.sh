9502c2daa672e1809bfafedb27c042b56ec06908
b915427386 <b915427386@gmail.com>
2018-09-25 18:31:17 +0800

    aws

---
+++ build_service.sh
@@ -0,0 +1,17 @@
+
+#!/bin/bash
+echo "Logging in to AWS ECR and pull images "
+$(aws ecr get-login --no-include-email --region ap-northeast-1)
+docker pull 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/api:latest
+docker pull 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/mysql:latest
+docker pull 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/redis:latest
+docker pull 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/jupyter:0.0.2
+docker pull 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/ngrok:latest
+echo "download S3 cc102-csv data "
+cd /home/ec2-user/deploy
+git submodule init 
+git submodule update
+cd Chatbot_Dev/mysql_init/
+aws s3 cp s3://cc102-csv/SA.csv ./
+aws s3 cp s3://cc102-csv/SYS.csv ./
+aws s3 cp s3://cc102-csv/Develope.csv ./
---
+++ stop_service.sh
@@ -0,0 +1,19 @@
+
+#!/bin/bash
+sudo chown ec2-user.ec2-user /home/ec2-user/deploy -Rf
+cd /home/ec2-user/deploy
+echo "remove service container "
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+echo "re-tag image "
+docker tag 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/api:latest 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/api:older
+docker tag 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/mysql:latest 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/mysql:older
+docker tag 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/redis:latest 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/redis:older
+docker tag 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/jupyter:latest 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/jupyter:older
+docker tag 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/ngrok:latest 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/ngrok:older
+echo "remove image first "
+docker rmi 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/api:latest
+docker rmi 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/redis:latest
+docker rmi 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/mysql:latest
+docker rmi 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/jupyter:latest
+docker rmi 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/ngrok:latest
